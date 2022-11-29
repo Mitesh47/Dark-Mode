1896e6a9fb350977bc888dcc7ba8e61f36944601
Mitesh <miteshmor47@gmail.com>
2022-11-29 17:32:12 +0530

    User interface

---
+++ src/app/models/userDB.ts
@@ -0,0 +1,10 @@
+export interface User{
+  uid?: number;
+  displayName?: string;
+  email?: string;
+  photoURL?: string;
+  firstName?: string;
+  lastName?: string;
+  address?: string;
+  phone?: string;
+}
